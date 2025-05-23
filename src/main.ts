99d7d1fd915f2d41d91bba8ec3a55af4595f05b0
nprdservice7 <nprdservice7@gmail.com>
2025-05-23 11:29:44 +0545

    feat:Added the swagger ui to the project

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ApiConfigService } from './config/api-config.service';
+import { DocumentBuilder, SwaggerCustomOptions, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,8 +9,25 @@ async function bootstrap() {
   app.enableCors();
   const apiConfigService = app.get(ApiConfigService)
 
+    const customOptions: SwaggerCustomOptions = {
+    swaggerOptions: { docExpansion: 'none', persistAuthorization: true },
+    customCss: '.swagger-ui .topbar {display: none}',
+    customSiteTitle: apiConfigService.getValue('API_TITLE'),
+  };
+
+  const options = new DocumentBuilder()
+    .setTitle(apiConfigService.getValue('API_TITLE'))
+    .setDescription(apiConfigService.getValue('API_DESCRIPTION'))
+    .setVersion(apiConfigService.getValue('API_VERSION'))
+    .addBearerAuth()
+    .addServer(
+      `${apiConfigService.getValue('HOST')}:${apiConfigService.getValue('PORT')}`,
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/docs', app, document, customOptions);
   console.log(
-    `Api Started in ${apiConfigService.getValue('HOST')}:${apiConfigService.getValue('PORT')}`,
+    `App started in ${apiConfigService.getValue('HOST')}:${apiConfigService.getValue('PORT')}`,
   );
 
   await app.listen(apiConfigService.getValue('PORT'));
